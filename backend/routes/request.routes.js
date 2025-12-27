9c8d58a275b636dc9d12fc5aae2e9c79de70e92b
Pawan Tiwari <ramtiwari7081@gmail.com>
2025-12-27 13:30:43 +0530

    added /POST Request logic in routes

---
+++ backend/routes/request.routes.js
@@ -0,0 +1,48 @@
+import express from "express";
+import Equipment from "../models/Equipment.js";
+import Request from "../models/Request.js";
+import mongoose from "mongoose";
+
+const router = express.Router();
+
+/* CREATE REQUEST */
+router.post("/", async (req, res) => {
+  try {
+    const { subject, equipmentId, type, scheduledDate } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(equipmentId)) {
+      return res.status(400).json({ message: "Invalid equipment id" });
+    }
+
+    const equipment = await Equipment.findById(equipmentId);
+    if (!equipment) {
+      return res.status(404).json({ message: "Equipment not found" });
+    }
+
+    const request = await Request.create({
+      subject,
+      equipment: equipment._id,
+      team: equipment.maintenanceTeam, // 🔥 AUTO
+      type,
+      scheduledDate,
+    });
+
+    res.status(201).json(request);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/* UPDATE STATUS (KANBAN) */
+router.patch("/:id/status", async (req, res) => {
+  const { status, hoursSpent } = req.body;
+
+  const request = await Request.findById(req.params.id);
+  request.status = status;
+  if (hoursSpent) request.hoursSpent = hoursSpent;
+
+  await request.save();
+  res.json(request);
+});
+
+export default router;
